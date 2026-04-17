3c8bd25cf233338155363d4e278ba5b2900c47cc
dantezerega <dante.other@gmail.com>
2026-04-17 11:15:14 -0400

    added css

--- firstsite.js
+++ firstsite.js
@@ -1,5 +1,6 @@
 const heading = document.createElement("h1");
 heading.textContent = "It works!";
+heading.id = "title";
 document.body.appendChild(heading);
 
 console.log("Script loaded successfully");
